1584525767c27275bec7fd136792ff910063cd77
Md Maksud Hossain Pranto <43799953+maksudpranto@users.noreply.github.com>
2021-06-14 20:15:45 +0600

    Add files via upload

---
+++ Simple_Unit_Test_Login.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import unittest
+
+
+class LoginTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+
+    def test_login_valid(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.find_element_by_id('txtUsername').send_keys('Admin')
+        self.driver.find_element_by_id('txtPassword').send_keys('admin123')
+        self.driver.find_element_by_id('btnLogin').click()
+        self.driver.find_element_by_id('welcome').click()
+        self.driver.find_element_by_link_text('Logout').click()
+        time.sleep(2)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("Test Completed")
+
+
+if __name__ == "__main__":
+    unittest.main()
